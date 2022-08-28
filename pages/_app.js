200dc1ed12b95cea571092859d83a7b7bbf6c2e0
OzanOcak <ozanocak@gmail.com>
2022-08-28 09:32:37 -0400

    styling with styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,27 @@
 import Link from "next/link";
 import styled from "styled-components";
 import { Normalize } from "styled-normalize";
+import Navbar from "../components/Navbar";
 
 const Container = styled.div`
   @import url("https://fonts.googleapis.com/css2?family=DynaPuff:wght@400;700&display=swap");
 
   background: lightblue;
   font-family: "DynaPuff", cursive;
+  color: #444;
+`;
+
+const Page = styled.div`
+  width: 100%;
+  max-width: 768px;
+  margin: 0 auto;
 `;
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Container>
       <Normalize />
+      <Navbar />
       <Link href="/">
         <a>Home</a>
       </Link>
--- pages/index.js
+++ pages/index.js
@@ -2,25 +2,56 @@ import Link from "next/link";
 import fs from "fs";
 import matter from "gray-matter";
 import styled from "styled-components";
+import UnstyledLink from "../components/styled/UnstyledLink";
 
 const Container = styled.div`
-  background: lightgray;
+  background: white;
+  padding: 1rem 2rem;
+  min-height: 200px;
+  position: relative;
+  transition: transform 0.3s;
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
+const ProductsContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0.9rem;
+  margin: 0.6rem 0.9rem;
+`;
+
+const Price = styled.div`
+  position: absolute;
+  bottom: 0.5rem;
+  right: 1rem;
+  font-size: 2.5rem;
+`;
+
+const renderProduct = (product) => {
+  const handleClick = (e) => {
+    e.stopPropagation();
+  };
+
+  return (
+    <Link key={product.id} href={product.slug}>
+      <UnstyledLink>
+        <Container>
+          <h1>{product.name}</h1>
+          <p>{product.description}</p>
+          <button onClick={handleClick}>Add to cart</button>
+          <Price>${product.price / 100}</Price>
+        </Container>
+      </UnstyledLink>
+    </Link>
+  );
+};
+
 const HomePage = (props) => {
-  return props.products.map((product) => {
-    return (
-      <Container>
-        <Link href={product.slug}>
-          <a>
-            <h1>{product.name}</h1>
-          </a>
-        </Link>
-        <p>{product.description}</p>
-        <p>${product.price / 100}</p>
-      </Container>
-    );
-  });
+  return (
+    <ProductsContainer>{props.products.map(renderProduct)}</ProductsContainer>
+  );
 };
 
 export const getStaticProps = async () => {
